520148a4ce14c1edf19fdbd5cb309d9a8ae3f526
Lasse Ranum <lasseran@stud.ntnu.no>
2025-02-06 11:33:59 +0100

    feat: :sparkles: Add login component
    Added a component for login functionality, including email/password and google authentication.

--- futleie-app/src/App.tsx
+++ futleie-app/src/App.tsx
@@ -1,39 +1,41 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import { createClient } from '@supabase/supabase-js'
-import {User} from './Types'
-const supabaseUrl = 'https://wcudjrwfxpqnytjdmojb.supabase.co'
-const supabaseKey = process.env.SUPABASE_DEV_KEY as string
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import { createClient } from "@supabase/supabase-js";
+import { User } from "./Types";
+const supabaseUrl = process.env.VITE_SUPABASE_URL as string;
+const supabaseKey = process.env.VITE_SUPABASE_DEV_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+    useEffect(() => {
+        getUsers();
+    }, []);
 
-  async function getUsers() {
-    const { data } = await supabase.from("Users").select();
-    if (data) {
-      setUsers(data as User[]);
+    async function getUsers() {
+        const { data, error } = await supabase.from("Users").select("*");
+        if (data) {
+            setUsers(data as User[]);
+            console.log("Users: ", data);
+            console.log("Error: ", error);
+        }
     }
-  }
 
-  return (
-    <div className="mt-4 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div className="flex-shrink-0">
-        <img className="h-12 w-12" src={reactLogo} alt="React logo" />
-      </div>
-      <div>
-        <div className="text-xl font-medium text-black">Users</div>
-        <ul>
-        {users.map((user) => (
-          <li key={user.username}>{user.username}</li>
-        ))}
-      </ul>
-      </div>
-    </div>
-  )
+    return (
+        <div className="mt-4 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            <div className="flex-shrink-0">
+                <img className="h-12 w-12" src={reactLogo} alt="React logo" />
+            </div>
+            <div>
+                <div className="text-xl font-medium text-black">Users</div>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.username}>{user.username}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
-export default App
+export default App;
