f729f6ae230349da19f553d1a1a2912c522b5bda
Lasse Ranum <lasseran@stud.ntnu.no>
2025-02-07 10:54:50 +0100

    refactor: :art: Endret hvordan sign-up styling er strukturert
    Sørget for at app/signup/page.tsx er komponenten som importeres, ikke selve sign-up komponenten.  Co-author: Håkon Gray

--- futleie-app/src/main.tsx
+++ futleie-app/src/main.tsx
@@ -4,15 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import Page from "./app/login/page.tsx";
-import Signup from "./components/signup-form.tsx";
+import Signup from "./app/signup/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
             <Routes>
-                <Route path="/" element={<App/>}></Route>
-                <Route path="/login" element={<Page/>}></Route>
-                <Route path="/signup" element={<Signup/>}></Route>
+                <Route path="/" element={<App />}></Route>
+                <Route path="/login" element={<Page />}></Route>
+                <Route path="/signup" element={<Signup />}></Route>
             </Routes>
         </Router>
     </StrictMode>
