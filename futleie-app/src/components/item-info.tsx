2c8e42acb38e0c4d0a69d635450daffde4343f1f
Lasse Ranum <lasseran@stud.ntnu.no>
2025-02-14 12:27:14 +0100

    feat: :sparkles: Legg til "Endre annonse" knapp
    Lagt til endre annnonse knapp som vises kun for eieren av utleieobjektet. Co-author: tuvakgr@stud.ntnu.no

--- futleie-app/src/components/item-info.tsx
+++ futleie-app/src/components/item-info.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 import { useParams } from "react-router-dom";
 import supabaseClient from "@/supabaseClient";
 import { Item } from "@/Types/Item";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
@@ -15,121 +16,153 @@ import "react-day-picker/dist/style.css";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
 const ItemInfo: React.FC = () => {
-  const { itemId } = useParams<{ itemId: string }>();
-  const [item, setItem] = useState<Item | null>(null);
-  const [images, setImages] = useState<Array<string> | null>(null);
-  const [dateRange, setDateRange] = useState<DateRange | undefined>({
-    from: new Date(),
-    to: undefined,
-  });
-  /*
-   * Hook som henter item fra databasen basert på itemId og setter item state
-   * @returns void
-   */
-  useEffect(() => {
-    const fetchItem = async () => {
-      console.log("Fetching item with ID:", itemId);
-      const { data, error } = await supabaseClient
-        .from("Items")
-        .select()
-        .eq("id", itemId)
-        .single();
+    const { itemId } = useParams<{ itemId: string }>();
+    const [item, setItem] = useState<Item | null>(null);
+    const [images, setImages] = useState<Array<string> | null>(null);
+    const [dateRange, setDateRange] = useState<DateRange | undefined>({
+        from: new Date(),
+        to: undefined,
+    });
+    const [currentUserId, setCurrentUserId] = useState<number | null>(null);
 
-      if (error) {
-        console.error("Error fetching item:", error);
-      } else {
-        console.log("Fetched item:", data);
-        setItem(data as Item);
-        console.log("Item is set to: ", data);
-      }
-    };
+    useEffect(() => {
+        // Hent bruker fra cookie
+        const userCookie = Cookies.get("user");
+        if (userCookie) {
+            const parsedUser = JSON.parse(userCookie);
 
-    if (itemId) {
-      fetchItem();
-    }
-  }, [itemId]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      if (item) {
-        console.log("Fetching images");
-        const { data, error } = await supabaseClient
-          .from("Item_images")
-          .select("image_url")
-          .eq("item_id", itemId);
-
-        if (error) {
-          console.log("Error fetching images");
-          return;
+            if (parsedUser.id) {
+                setCurrentUserId(parsedUser.id);
+            }
         }
-        const fetchedImages = data.map((image) => image.image_url);
-        console.log(fetchedImages);
-        setImages(fetchedImages);
-      }
-    };
-    fetchImages();
-  }, [itemId, item]);
+    }, []);
 
-  if (!item || !images) {
-    return <p>Loading...</p>;
-  }
+    useEffect(() => {
+        const fetchItem = async () => {
+            const { data, error } = await supabaseClient
+                .from("Items")
+                .select()
+                .eq("id", itemId)
+                .single();
 
-  return (
-    <div className="flex flex-col items-center gap-6 m-5">
-      <div className="flex flex-col gap-6 w-full max-w-2xl">
-        <div className="flex flex-col gap-4">
-          <Carousel className="w-full h-100">
-            <CarouselContent>
-              {images.map((imageUrl, index) => (
-                <CarouselItem key={index}>
-                  <img
-                    src={imageUrl}
-                    alt={`Item Image ${index}`}
-                    className="w-full h-100"
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
-          <h1 className="text-3xl font-bold">{item.title}</h1>
-          <p className="text-xl">{item.description}</p>
-          <label className="text-lg text-gray-600">Leieperiode:</label>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                className="w-full justify-start text-left font-normal"
-              >
-                {dateRange?.from ? (
-                  dateRange.to ? (
-                    <>
-                      {format(dateRange.from, "LLL dd, y")} -{" "}
-                      {format(dateRange.to, "LLL dd, y")}
-                    </>
-                  ) : (
-                    format(dateRange.from, "LLL dd, y")
-                  )
-                ) : (
-                  <span>Velg en dato</span>
-                )}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                initialFocus
-                mode="range"
-                defaultMonth={dateRange?.from}
-                selected={dateRange}
-                onSelect={setDateRange}
-                numberOfMonths={3}
-              />
-            </PopoverContent>
-          </Popover>
+            if (error) {
+                console.error("Error fetching item:", error);
+            } else {
+                setItem(data as Item);
+            }
+        };
+
+        if (itemId) {
+            fetchItem();
+        }
+    }, [itemId]);
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            if (item) {
+                const { data, error } = await supabaseClient
+                    .from("Item_images")
+                    .select("image_url")
+                    .eq("item_id", itemId);
+
+                if (error) {
+                    console.log("Error fetching images");
+                    return;
+                }
+                const fetchedImages = data.map((image) => image.image_url);
+                console.log(fetchedImages);
+                setImages(fetchedImages);
+            }
+        };
+        fetchImages();
+    }, [itemId, item]);
+
+    if (!item || !images) {
+        return <p>Loading...</p>;
+    }
+
+    const isOwner = item.owner_id === currentUserId;
+
+    return (
+        <div className="flex flex-col items-center gap-6 m-5">
+            <div className="flex flex-col gap-6 w-full max-w-2xl">
+                <div className="flex flex-col gap-4">
+                    <Carousel className="w-full h-100">
+                        <CarouselContent>
+                            {images.map((imageUrl, index) => (
+                                <CarouselItem key={index}>
+                                    <img
+                                        src={imageUrl}
+                                        alt={`Item Image ${index}`}
+                                        className="w-full h-100"
+                                    />
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                    </Carousel>
+                    <h1 className="text-3xl font-bold">{item.title}</h1>
+                    <p className="text-xl">{item.description}</p>
+
+                    {isOwner ? (
+                        // Hvis eier
+                        <Button variant="outline">Endre annonse</Button>
+                    ) : (
+                        // Hvis ikke eier
+                        <>
+                            <label className="text-lg text-gray-600">
+                                Leieperiode:
+                            </label>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant="outline"
+                                        className="w-full justify-start text-left font-normal"
+                                    >
+                                        {dateRange?.from ? (
+                                            dateRange.to ? (
+                                                <>
+                                                    {format(
+                                                        dateRange.from,
+                                                        "LLL dd, y"
+                                                    )}{" "}
+                                                    -{" "}
+                                                    {format(
+                                                        dateRange.to,
+                                                        "LLL dd, y"
+                                                    )}
+                                                </>
+                                            ) : (
+                                                format(
+                                                    dateRange.from,
+                                                    "LLL dd, y"
+                                                )
+                                            )
+                                        ) : (
+                                            <span>Velg en dato</span>
+                                        )}
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent
+                                    className="w-auto p-0"
+                                    align="start"
+                                >
+                                    <Calendar
+                                        initialFocus
+                                        mode="range"
+                                        defaultMonth={dateRange?.from}
+                                        selected={dateRange}
+                                        onSelect={setDateRange}
+                                        numberOfMonths={3}
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                            <Button>Send forespørsel</Button>
+                        </>
+                    )}
+                </div>
+            </div>
         </div>
-        <Button>Send forespørsel</Button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ItemInfo;
