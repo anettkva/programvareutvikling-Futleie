771c5bdad8cd04d46f882ea0596bb5d86e8d77e1
Lasse Ranum <lasseran@stud.ntnu.no>
2025-02-14 12:56:48 +0100

    fix: :bug: Legg til routing i App
    Lagt til routing til change-ad i App.tsx. Co-author: hngray@stud.ntnu.no

--- futleie-app/src/App.tsx
+++ futleie-app/src/App.tsx
@@ -7,47 +7,53 @@ import CreateAdPage from "./pages/create-ad";
 import Cookie from "js-cookie";
 import ProfilePage from "./pages/profile-page";
 import ItemInfo from "./components/item-info";
+import ChangeItemForm from "./components/change-item-form";
+import ChangeAd from "./pages/change-ad";
 
 const App: React.FC<{}> = () => {
-  const location = useLocation();
-  const userCookie = Cookie.get("user");
-  const isAuthenticated = userCookie && userCookie.length > 0;
+    const location = useLocation();
+    const userCookie = Cookie.get("user");
+    const isAuthenticated = userCookie && userCookie.length > 0;
 
-  // Allow access to login and signup pages even when not authenticated
-  if (!isAuthenticated && !["/login", "/signup"].includes(location.pathname)) {
-    return <Navigate to="/login" replace />;
-  }
+    // Allow access to login and signup pages even when not authenticated
+    if (
+        !isAuthenticated &&
+        !["/login", "/signup"].includes(location.pathname)
+    ) {
+        return <Navigate to="/login" replace />;
+    }
 
-  // Redirect authenticated users away from login/signup to gallery
-  if (isAuthenticated && ["/login", "/signup"].includes(location.pathname)) {
-    return <Navigate to="/gallery" replace />;
-  }
+    // Redirect authenticated users away from login/signup to gallery
+    if (isAuthenticated && ["/login", "/signup"].includes(location.pathname)) {
+        return <Navigate to="/gallery" replace />;
+    }
 
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/gallery" replace />} />
-      <Route path="/login" element={<Page />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route
-        path="/gallery"
-        element={
-          <Layout>
-            <Gallery />
-          </Layout>
-        }
-      />
-      <Route path="/create-ad" element={<CreateAdPage />} />
-      <Route
-        path="/profile"
-        element={
-          <Layout>
-            <ProfilePage />
-          </Layout>
-        }
-      />
-      <Route path="/item/:itemId" element={<ItemInfo />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/gallery" replace />} />
+            <Route path="/login" element={<Page />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+                path="/gallery"
+                element={
+                    <Layout>
+                        <Gallery />
+                    </Layout>
+                }
+            />
+            <Route path="/create-ad" element={<CreateAdPage />} />
+            <Route
+                path="/profile"
+                element={
+                    <Layout>
+                        <ProfilePage />
+                    </Layout>
+                }
+            />
+            <Route path="/item/:itemId" element={<ItemInfo />} />
+            <Route path="/change-ad/:itemId" element={<ChangeAd />} />
+        </Routes>
+    );
 };
 
 export default App;
