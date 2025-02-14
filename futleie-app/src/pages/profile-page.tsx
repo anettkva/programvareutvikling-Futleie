d9943b027b98c24d1f3b56b9217a45ce21d1c4fa
Lasse Ranum <lasseran@stud.ntnu.no>
2025-02-14 10:24:10 +0100

    feat: :art: Fiks responsivitet
    Fikset responsivt design på ProfilePage. co-author: tuvakgr@ntnu.no

--- futleie-app/src/pages/profile-page.tsx
+++ futleie-app/src/pages/profile-page.tsx
@@ -3,11 +3,11 @@ import Profile from "@/components/profile";
 
 export default function ProfilePage() {
     return (
-        <div className="flex w-full gap-4">
-            <div className="w-1/2">
+        <div className="flex flex-col 2xl:flex-row w-full gap-4">
+            <div className="2xl:w-1/2 w-full">
                 <Profile />
             </div>
-            <div className="w-1/2">
+            <div className="2xl:w-1/2 w-full">
                 <h1 className="text-3xl font-bold mb-6 px-5">Mine annonser</h1>
                 <ItemGrid />
             </div>
