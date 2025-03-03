822d1bb773cbcddfb6f2b2c46adac7a7a2e60edc
Lasse Ranum <lasseran@stud.ntnu.no>
2025-03-03 09:02:34 +0100

    fix: :bug: Fiks problem med bilderendering
    Endret måten bilder og eiere hentes til at det gjøres før de sendes inn til Item-Grid komponenten, slik at man unngår trøbbel med flere queries som skaper flimring senere. Co-author: karlkf@stud.ntnu.no

--- futleie-app/src/components/item-grid.tsx
+++ futleie-app/src/components/item-grid.tsx
@@ -6,91 +6,56 @@ import { useNavigate } from "react-router-dom";
 import { putImagesInItems, putOwnersInItems } from "./itemfilling";
 
 type ItemGridProps = {
-  inputItems: Item[];
+    inputItems: Item[];
 };
 
 const ItemGrid: React.FC<ItemGridProps> = ({ inputItems = [] }) => {
-  const [items, setItems] = useState<Item[] | null>(inputItems);
-  const navigate = useNavigate();
+    const [items, setItems] = useState<Item[] | null>(inputItems);
+    const navigate = useNavigate();
 
-  // Ref-er som brukes for å sørge for at hookene stopper når de skal
-  const setImageLimiter = useRef<boolean>(false);
-  const setOwnerLimiter = useRef<boolean>(false);
+    const handleCreateAd = () => {
+        navigate("/create-ad");
+    };
 
-  const handleCreateAd = () => {
-    navigate("/create-ad");
-  };
+    useEffect(() => {
+        setItems(inputItems);
+    }, [inputItems]);
 
-  useEffect(() => {
-    setItems(inputItems);
-    setImageLimiter.current = false;
-    setOwnerLimiter.current = false;
-  }, [inputItems]);
-
-  // Setter bilder inn i itemsene som ble fetchet i hooken ovenfor
-  useEffect(() => {
-    if (!items) return;
-
-    // Brukes for å hindre at hooken kjører i endeløs loop
-    if (setImageLimiter.current) return;
-
-    putImagesInItems(items, setItems);
-
-    // Oppdaterer Ref-en slik at hooken slutter å kjøre når items har fått inn bilder
-    if (items && items[0] && items[0].images) {
-      setImageLimiter.current = true;
-    }
-  }, [items]);
-
-  // Setter eiere inn i itemsene som ble fetchet i hooken ovenfor
-  useEffect(() => {
-    if (!items) return;
-
-    // Brukes for å hindre at hooken kjører i endeløs loop
-    if (setOwnerLimiter.current) return;
-
-    // Se dokumentasjon i itemfilling.ts
-    putOwnersInItems(items, setItems);
-
-    // Oppdaterer Ref-en slik at hooken slutter å kjøre når items har fått inn eiere
-    if (items && items[0] && items[0].owner) {
-      setOwnerLimiter.current = true;
-    }
-  }, [items]);
-
-  return (
-    <div>
-      <div className="flex justify-end px-5 pt-5">
-        <Button
-          onClick={handleCreateAd}
-          className="bg-black hover:bg-gray-800 text-white"
-        >
-          Opprett annonse
-        </Button>
-      </div>
-      <div className="grid gap-6 m-5 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]">
-        {items ? (
-          items.map((item) => {
-            return (
-              <ItemCard
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                // Tar første bilde som forsidebilde
-                imageUrl={
-                  item.images && item.images.length > 0 ? item.images[0] : ""
-                }
-                owner={item.owner ? item.owner : ""}
-                className="col-span-1"
-              />
-            );
-          })
-        ) : (
-          <p>No items found</p>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex justify-end px-5 pt-5">
+                <Button
+                    onClick={handleCreateAd}
+                    className="bg-black hover:bg-gray-800 text-white"
+                >
+                    Opprett annonse
+                </Button>
+            </div>
+            <div className="grid gap-6 m-5 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]">
+                {items ? (
+                    items.map((item) => {
+                        return (
+                            <ItemCard
+                                key={item.id}
+                                id={item.id}
+                                title={item.title}
+                                // Tar første bilde som forsidebilde
+                                imageUrl={
+                                    item.images && item.images.length > 0
+                                        ? item.images[0]
+                                        : ""
+                                }
+                                owner={item.owner ? item.owner : ""}
+                                className="col-span-1"
+                            />
+                        );
+                    })
+                ) : (
+                    <p>No items found</p>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default ItemGrid;
--- futleie-app/src/components/searchable-item-grid.tsx
+++ futleie-app/src/components/searchable-item-grid.tsx
@@ -5,38 +5,74 @@ import supabaseClient from "@/supabaseClient";
 import { Item } from "@/Types/Item";
 
 const SearchableItemGrid: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState<Item[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState<Item[]>([]);
 
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (searchTerm.trim() === "") {
-        setSearchResults([]);
-        return;
-      }
+    useEffect(() => {
+        const fetchSearchResults = async () => {
+            if (searchTerm.trim() === "") {
+                const { data, error } = await supabaseClient
+                    .from("Items")
+                    .select(
+                        `
+                      *,
+                      Item_images(image_url),
+                      Users:owner_id(username)
+                      `
+                    );
+                if (error) {
+                    console.error("Error fetching search results:", error);
+                } else {
+                    // Transform the data to match your Item type structure
+                    const formattedResults = data.map((item) => ({
+                        ...item,
+                        images: item.Item_images.map(
+                            (img: { image_url: any }) => img.image_url
+                        ),
+                        owner: item.Users?.username || "",
+                    }));
+                    setSearchResults(formattedResults);
+                }
+                return;
+            }
 
-      const { data, error } = await supabaseClient
-        .from("Items")
-        .select("*")
-        .or(`title.ilike.%${searchTerm}%,description.ilike.%${searchTerm}%`);
+            const { data, error } = await supabaseClient
+                .from("Items")
+                .select(
+                    `
+          *,
+          Item_images(image_url),
+          Users:owner_id(username)
+        `
+                )
+                .or(
+                    `title.ilike.%${searchTerm}%,description.ilike.%${searchTerm}%`
+                );
 
-      if (error) {
-        console.error("Error fetching search results:", error);
-      } else {
-        console.log("Search results:", data);
-        setSearchResults(data);
-      }
-    };
+            if (error) {
+                console.error("Error fetching search results:", error);
+            } else {
+                // Transform the data to match your Item type structure
+                const formattedResults = data.map((item) => ({
+                    ...item,
+                    images: item.Item_images.map(
+                        (img: { image_url: any }) => img.image_url
+                    ),
+                    owner: item.Users?.username || "",
+                }));
+                setSearchResults(formattedResults);
+            }
+        };
 
-    fetchSearchResults();
-  }, [searchTerm]);
+        fetchSearchResults();
+    }, [searchTerm]);
 
-  return (
-    <div>
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <ItemGrid inputItems={searchResults} />
-    </div>
-  );
+    return (
+        <div>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            <ItemGrid inputItems={searchResults} />
+        </div>
+    );
 };
 
 export default SearchableItemGrid;
