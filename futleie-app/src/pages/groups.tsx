22d7ba16aa58080f98db322a0c47246679f15086
Oskar Fridstrøm <oskar@fridstrom.no>
2025-03-10 10:41:08 +0100

    .

--- futleie-app/src/pages/groups.tsx
+++ futleie-app/src/pages/groups.tsx
@@ -4,10 +4,6 @@ export default function Groups() {
     return (
         <div className="flex h-screen">
             <AppSidebar />
-            <div className="flex-1 p-4">
-                <h1 className="text-2xl font-bold mb-4">Groups</h1>
-                <p>Group content coming soon...</p>
-            </div>
         </div>
     );
 }
